2e479db358ba9f181c437ac3975d65cba243eb8b
Daniil Orel <oreldan08@gmail.com>
2021-02-04 12:19:09 +0300

    hw 15

---
+++ hw15/router.py
@@ -0,0 +1,149 @@
+"""
+    Написать класс router.
+    Должен иметь методы добавить/удалить/вывести список ip address.
+    Должен иметь методы добавить/удалить/вывести список ip routes.
+
+    Есть маршруты к непосредственно-подключенным сетям:
+    если у устройства есть ip-adress 192.168.5.14/24 на интерфейсе eth1,
+    значит у него должен быть маршрут:
+    к сети 192.168.5.0/24 через eth1 или через 192.168.5.14.
+
+    Если мы хотим добавить маршрут к какой-нибудь удаленной сети,
+    то надо проверять доступен ли gateway.
+
+    Например мы можем добавить маршрут к 172.16.0.0/16 через gateway
+    192.168.5.132, только если у нас уже есть маршрут до 192.168.5.132.
+
+    Если же мы попытаемся добавить маршрут до какой-либо сети через gateway,
+    до которого у нас пока еще нет маршрута, то должен вылетать exception.
+
+    Например:
+    Добавляем ip-address 192.168.5.14/24 eth1.
+    Добавляем маршрут до 172.16.0.0/16 через 192.168.5.1 - ok.
+    Добавляем маршрут до 172.24.0.0/16 через 192.168.8.1 - exception.
+    Добавляем маршрут до 172.24.0.0/16 через 172.16.8.1 - ok.
+
+    Итого - 1 интерфейс и 3 маршрута в таблице.
+"""
+
+import ipaddress as ip
+from functools import wraps
+
+
+def add_ip_address_decorator(add_ip_address):
+    @wraps(add_ip_address)
+    def wrapper(self, ip_address, interface_name):
+        print(f"Adding ip-address {ip_address} {interface_name}.")
+        add_ip_address(self, ip_address, interface_name)
+
+    return wrapper
+
+def remove_ip_address_decorator(remove_ip_address):
+    @wraps(remove_ip_address)
+    def wrapper(self, interface_name):
+        ip_address = remove_ip_address(self, interface_name)
+
+        if ip_Address is not None:
+            print(f"Removing ip-address {ip_address} {interface_name}")
+
+    return wrapper
+
+def add_route_decorator(add_route):
+    @wraps(add_route)
+    def wrapper(self, ip_addr, gateway):
+        res = add_route(self, ip_addr, gateway)
+        str_res = f"Adding route to {ip_addr} through {gateway} - "
+
+        if res is not None:
+            str_res += "ok"
+        else:
+            str_res += "exception"
+
+        print(str_res)
+    
+    return wrapper
+
+def remove_route_decorator(remove_route):
+    @wraps(remove_route)
+    def wrapper(self, network_ip):
+        route = remove_route(self, network_ip)
+
+        if route is not None:
+            print(f"Removing route to {route[0]} through {route[1]}")
+
+    return wrapper
+
+
+class Router:
+
+    def __init__(self):
+        # Словарь формата <interface_name>:<ip_address>
+        self.ip_addresses = {}
+        # Элементы списка - кортежи формата (network_ip, connection_ip_address)
+        # network_ip - сеть, до которой ведет маршрут
+        # connection_ip_address - ip адрес маршрутизатора из ip_addresses
+        self.routes = []
+
+    @add_ip_address_decorator
+    def add_ip_address(self, ip_address, interface_name):
+        interface = ip.IPv4Interface(ip_address)
+
+        self.ip_addresses.update({interface_name: interface.ip})
+        # Добавляем маршрут к непосредственно подключенной сети
+        self.routes.append((interface.network, interface.ip))
+
+    @remove_ip_address_decorator
+    def remove_ip_address(self, interface_name):
+        try:
+            # Поиск маршрута, для которого используется удаляемый ip-адрес
+            route = [x[1] for x in self.routes].index(self.ip_addresses[interface_name])
+            
+            self.routes.pop(route)
+            return self.ip_addresses.pop(interface_name)
+        except ValueError:
+            return None
+
+    def show_ip_addresses(self):
+        print("IP Addresses:\nIF:\tIP:")
+        
+        for k,v in self.ip_addresses.items():
+            print(f"{k}\t{v}")
+
+    @add_route_decorator
+    def add_ip_route(self, ip_address_str, gateway_str):
+        gateway = ip.ip_address(gateway_str)
+        ip_address = ip.ip_network(ip_address_str)
+
+        for elem in self.routes:
+            hosts = list(elem[0].hosts())
+            if gateway in hosts and gateway != elem[1]:
+                self.routes.append((ip_address, gateway))
+
+                return (ip_address, gateway)
+
+    @remove_route_decorator
+    def remove_ip_route(self, network_ip_str):
+        network_ip = ip.ip_network(network_ip_str)
+
+        try:
+            i = [x[0] for x in self.routes].index(network_ip)
+            return self.routes.pop(i)
+        except ValueError:
+            return None
+        
+    def show_ip_routes(self):
+        counter = 1
+        print("IP Routes:\nNum:\tIPRANGE:\tNextHopIPAddress")
+
+        for elem in self.routes:
+            print(f"{counter}:\t{elem[0]}\t{elem[1]}")
+            counter += 1
+
+
+r1 = Router()
+r1.add_ip_address('192.168.5.14/24', "eth1")
+r1.add_ip_route('172.16.0.0/16', '192.168.5.1') # ok
+r1.add_ip_route('172.16.0.0/16', '172.168.8.1') # exception
+r1.add_ip_route('172.24.0.0/16', '172.16.8.1')  # ok
+r1.show_ip_addresses()
+r1.show_ip_routes()
--- hw7.py
+++ hw7.py
@@ -1,6 +1,6 @@
-# TODO:
-# 1. Второй пункт задания
-
+"""
+    Результат работы программы находится в файле /files/output.txt
+"""
 
 # Чтение файла и преобразование файла в двумерный список
 def read_file_line_by_line(path):
@@ -35,7 +35,7 @@ def count_users(column):
     return count_dict
 
 # Формируем строку из словаря и записываем ее в файл
-def print_result_to_file(res_dict):
+def print_result_to_file_1(res_dict):
     final_str = "("
 
     for v, k in res_dict.items():
@@ -46,14 +46,36 @@ def print_result_to_file(res_dict):
     with open("files/output.txt", "w") as f:
         f.write(final_str)
 
-def get_set_of_group_users(column):
-    users_list = []
-    
-    for elem in column:
-        if elem != "":
-            users_list.extend(elem.split(','))
 
-    return set(users_list)
+def get_dict_groups_with_users_uid(group, passwd):
+    res_dict = {}
+    
+    for elem in group:
+        group_name = elem[0]
+        group_id = elem[2]
+        users = elem[3].split(',')
+        users_uids = []
+
+        for elem_pswd in passwd:
+            if group_id == elem_pswd[3] or elem_pswd[0] in users:
+                users_uids.append(elem_pswd[2])
+        
+        res_dict.update({group_name: users_uids})
+
+    return res_dict
+
+def print_result_to_file_2(group_users_uids):
+    res_str = "("
+
+    for k, v in group_users_uids.items():
+        v_str = ','.join(v)
+        res_str += f" {k}:{v_str}, "
+
+    res_str += ")"
+
+    with open("files/output.txt", "a") as f:
+        f.write(f"\n{res_str}")
+
 
 
 def main():
@@ -63,17 +85,14 @@ def main():
     passwd = read_file_line_by_line(path_to_passwd)
     group = read_file_line_by_line(path_to_group)
 
+    # Оболчки пользователей
     passwd_shell = get_column(passwd, 6)
     shell_users = count_users(passwd_shell)
-    print_result_to_file(shell_users)
+    print_result_to_file_1(shell_users)
 
-    group_names_column = get_column(group, 0)
-    group_users_column = get_column(group, 3)
-    users_set = get_set_of_group_users(group_users_column)
-
-    print(group_names_column)
-    print(group_users_column)
-    print(users_set)
+    # uid-ы пользователей всех групп файла /etc/group
+    group_users_uid = get_dict_groups_with_users_uid(group, passwd)
+    print_result_to_file_2(group_users_uid)
 
 if __name__ == "__main__":
     main()
