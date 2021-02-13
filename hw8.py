517c5c974d1cde0aa6921b717697e67ba77f73cc
Daniil Orel <oreldan08@gmail.com>
2021-02-13 20:16:36 +0300

    add hw8

--- hw8.py
+++ hw8.py
@@ -1,13 +1,23 @@
-def parse(string, i=0):
-    if len(string) > i:
-        return string[i] + parse(string, i + 1)
-    
+def text_handler():
+    string = input("-> ")
+
+    if string == "cancel":
+        print("Bye!")
+    else:
+        try:
+            if (x := int(string)) % 2 == 0:
+                print(x // 2)
+            else:
+                print(x * 3 + 1)
+        except ValueError:
+            print("Не удалось преобразовать введенный текст в число.")
+        finally:
+            return text_handler()
+
 
 def main():
-    string = input()
-    print(parse(string))
-    
+    text_handler()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
