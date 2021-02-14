d247426be5edd1f01338c885bb6dc2182f9c451f
Daniil Orel <oreldan08@gmail.com>
2021-02-14 03:04:37 +0300

    fix hw21

--- hw21.py
+++ hw21.py
@@ -1,16 +1,48 @@
-import itertools
-
-
 def merge(iter1, iter2):
-    lst = list(itertools.chain(iter1, iter2))
+    try:
+        buf1 = next(iter1)
+    except StopIteration:
+        yield from iter2
 
-    for i in sorted(lst):
-        yield i
+        return
+
+    try:
+        buf2 = next(iter2)
+    except StopIteration:
+        yield buf1
+        yield from iter1
+
+        return
+
+    while True:
+        if buf1 < buf2:
+            yield buf1
+
+            try:
+                buf1 = next(iter1)
+            except StopIteration:
+                yield buf2
+                yield from iter2
+
+                break
+
+        else:
+            yield buf2
+
+            try:
+                buf2 = next(iter2)
+            except StopIteration:
+                yield buf1
+                yield from iter1
+
+                break
 
 
 def main():
-    gen = merge((x for x in range(1, 4)), (x for x in range(2, 5)))
-    print(list(gen))
+    print(list(merge((x for x in range(1, 5)), (x for x in range(2, 6)))))
+    print(list(merge((x for x in range(1, 1)), (x for x in range(2, 6)))))
+    print(list(merge((x for x in range(1, 5)), (x for x in range(2, 2)))))
+    print(list(merge((x for x in range(1, 10)), (x for x in range(2, 5)))))
 
 
 if __name__ == '__main__':
