b25a8fa640f76a1231d922570742a161d2500be5
Daniil Orel <oreldan08@gmail.com>
2021-03-06 17:04:04 +0300

    change hw18

--- hw18.py
+++ hw18.py
@@ -1,16 +1,22 @@
 from PIL import Image
+import os
 
 
 class Miniature:
 
     def __init__(self, image):
         self.image = image
+        self.default_path = 'files/photos/minis'
 
     def create(self, size):
         with Image.open(self.image) as img:
             img.thumbnail(size)
             img_name = self.image.split('/')[-1]
-            img.save(f'files/photos/minis/mini_{img_name}')
+
+            if not os.path.exists(self.default_path):
+                os.makedirs(self.default_path)
+
+            img.save(f'{self.default_path}/mini_{img_name}')
 
 
 def main():
